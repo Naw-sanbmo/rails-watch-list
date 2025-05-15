5554da2ec91f3820a57908caa268832f656570f4
Naw San <nawsanbmo@gmail.com>
2025-05-15 17:01:24 +0900

    something

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :movies, only: [:index]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
+# GET "lists"
+# A user can see the details of a given list and its name
+# GET "lists/42"
+# A user can create a new list
+# GET "lists/new"
+# POST "lists"
