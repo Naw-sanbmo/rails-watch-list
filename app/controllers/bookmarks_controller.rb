44018b8e5a1bd508d0fa64df5b440daf18a4772b
Naw San <nawsanbmo@gmail.com>
2025-05-16 16:44:03 +0900

    some changes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -19,7 +19,6 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
     redirect_to list_path(@bookmark.list), status: :see_other
-
   end
 
   private
